5f60de4ebcd123302b1bfe8e7b5cb2d1f259246f
Roman Maik <ramanmaik0@gmail.com>
2021-12-15 20:49:08 +0200

    drug&drop

---
+++ main.js
@@ -0,0 +1,33 @@
+const block = document.querySelector(".item")
+const placeholders = document.querySelectorAll(".placeholder")
+
+block.addEventListener("dragstart",(event)=>{
+    event.target.classList.add("hold")
+    setTimeout(()=>{
+        event.target.classList.add("hide")
+    },0)
+})
+
+block.addEventListener("dragend",(event)=>{
+    event.target.classList.remove("hold", "hide")
+})
+
+for(const placeholder of placeholders){
+    placeholder.addEventListener("dragover", dragover)
+    placeholder.addEventListener("dragenter", dragenter)
+    placeholder.addEventListener("dragleave", dragleave)
+    placeholder.addEventListener("drop", dragdrop)
+}
+
+function dragover (event){
+    event.preventDefault()
+}
+function dragenter (event){
+    event.target.classList.add("hovered")
+}
+function dragleave (event){
+    event.target.classList.remove("hovered")
+}
+function dragdrop (event){
+    event.target.append(block)
+}
